93e1a85ea1b40c1bf05e2a43b1a3c25c3c127828
JavanShen <1985486089@qq.com>
2022-04-17 14:48:19 +0800

    none

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
 import Vue from 'vue';
 import App from './App.vue';
+
 import router from './router';
+import permission from './permission';
+
 import store from './store';
 
+import Element from 'element-ui';
+import '@/theme/style/theme/index.css';
+
+import * as echarts from 'echarts';
+
+import '../mock/api';
+
 Vue.config.productionTip = false;
+Object.defineProperties(Vue.prototype, {
+  $echarts: {
+    get() {
+      return echarts;
+    }
+  }
+});
+
+Vue.use(Element);
 
 new Vue({
   router,
