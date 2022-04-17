f99df2afd54809700d711520e0749d3c39bc02ec
JavanShen <1985486089@qq.com>
2022-04-17 14:47:06 +0800

    404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <img style="width: 70%" :src="bg" alt="不好!你想要的页面找不到了">
+</template>
+
+<script>
+import bg from '@/assets/404/404.svg';
+export default {
+    name: 'NotFound',
+    data() {
+        return {
+            bg
+        };
+    },
+}
+</script>
