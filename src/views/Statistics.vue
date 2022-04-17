826e7f9d410e94efeaeda35f2d630d2c0e44678c
JavanShen <1985486089@qq.com>
2022-04-17 14:47:24 +0800

    数据页

---
+++ src/views/Statistics.vue
@@ -0,0 +1,14 @@
+<template>
+    <Bar></Bar>
+</template>
+
+<script>
+import {Bar} from '@/components/statistics';
+
+export default {
+    name: "Statistics",
+    components: {
+        Bar,
+    },
+}
+</script>
