05bb2c4591015767574c39a2d9da1f4e9247ba51
JavanShen <1985486089@qq.com>
2022-04-20 17:07:08 +0800

    添加并配置svg-sprite-loader

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,26 @@
-module.exports={
-    lintOnSave : false,
+const path=require('path');
+const resolve=(dir) => {
+    return path.join(__dirname, dir);
+}
+
+module.exports = {
+    lintOnSave: false,
+    chainWebpack: config=>{
+        config.module
+        .rule('svg')
+        .exclude.add(resolve('src/IconSvg'))
+        .end()
+
+        config.module
+        .rule('icons')
+        .test(/\.svg$/)
+        .include.add(resolve('src/IconSvg'))
+        .end()
+        .use('svg-sprite-loader')
+        .loader('svg-sprite-loader')
+        .options({
+            symbolId: 'icon-[name]'
+        })
+        .end()
+    }
 }
