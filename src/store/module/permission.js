d3da03e3bfbd030391517bf1dc30f2ecb4b53d78
JavanShen <1985486089@qq.com>
2022-04-21 21:56:14 +0800

    vuex新增路由模块

---
+++ src/store/module/permission.js
@@ -0,0 +1,57 @@
+import {fixedRoutes,asyncRoutes} from '@/router/routes.js'
+
+// 检查是否有权限
+function hasPermission(roles, route) {
+    if (route.meta?.roles) {
+        return roles.some(role => route.meta.roles.includes(role));
+    } else {
+        return true;
+    }
+}
+
+// 递归遍历进行路由筛选
+function getRoutes(roles,routes) {
+    let res=[]
+    routes.forEach(route=>{
+        if(route.children){
+            route.children = getRoutes(roles,route.children)
+        }
+
+        if(hasPermission(roles,route)){
+            res.push({...route})
+        }
+    })
+    return res
+}
+
+const state=() => {
+    return {
+        routes:[],
+        addRoutes:[]
+    }
+}
+
+const mutations={
+    setRoutes(state,routes){
+        state.addRoutes=routes
+        state.routes=fixedRoutes.concat(routes)
+    }
+}
+
+const actions={
+    generateRoutes({commit},roles){
+        return new Promise(resolve=>{
+            // admin角色可以访问所有路由
+            let accessedRoutes=roles.includes('admin')?asyncRoutes:getRoutes(roles,asyncRoutes)
+            commit('setRoutes',accessedRoutes)
+            resolve(accessedRoutes)
+        })
+    }
+}
+
+export default{
+    namespaced:true,
+    state,
+    mutations,
+    actions
+}
