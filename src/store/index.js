5c8add95fbe0c99ce0e8c4982baf3cdccc872872
JavanShen <1985486089@qq.com>
2022-04-25 11:31:53 +0800

    user模块分离,getters模块分离

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { loginByUserName, getUserInfo } from '@/api/login';
-import { setToken } from '@/utils/auth';
+
+import getters from './getters';
 
 Vue.use(Vuex);
 
@@ -15,64 +15,12 @@ const modules = req.keys().reduce((modules, module) => {
 
 export default new Vuex.Store({
   state: {
-    token: '',
-    userInfo: {
-      routes: [{
-        path: '/',
-        name: '首页',
-        class: 'home',
-      }, {
-        path: '/edit',
-        name: '创作',
-        class: 'edit',
-      }, {
-        path: '/statistics',
-        name: '统计',
-        class: 'statistics',
-      }]
-    },
-
     slideBarState: false,
   },
-  getters: {
-    isLogin: state => !!state.token,
-    isRoute: state => state.userInfo.role !== undefined,
-    menus: state => state.userInfo.routes,
-  },
+  getters,
   mutations: {
-    setToken(state, token) {
-      state.token = token;
-    },
-    setUserInfo(state, userInfo) {
-      state.userInfo = userInfo;
-    },
     toggleSlideBar(state) {
       state.slideBarState = !state.slideBarState;
-    }
-  },
-  actions: {
-    login({ commit }, data) {
-      return loginByUserName(data).then(res => {
-        let data = res.data;
-        if (data.code === 0) {
-          commit('setToken', data.data.token);
-          setToken(data.data.token);
-          return data.data.token;
-        } else {
-          return Promise.reject(data.msg);
-        }
-      })
-    },
-    getUserInfo({ commit }, token) {
-      return getUserInfo(token).then(res => {
-        let data = res.data;
-        if (data.code === 0) {
-          commit('setUserInfo', data.data);
-          return data.data;
-        } else {
-          return Promise.reject(data.msg);
-        }
-      })
     }
   },
   modules
---
+++ src/store/module/user.js
@@ -0,0 +1,52 @@
+//用户信息
+import { getToken, setToken } from '@/utils/auth'
+import { loginByUserName, getUserInfo } from '@/api/login'
+
+const state = () => {
+    return {
+        token: getToken(),
+        userInfo: {}
+    }
+}
+
+const mutations = {
+    setToken(state, token) {
+        state.token = token
+    },
+    setUserInfo(state, userInfo) {
+        state.userInfo = userInfo
+    }
+}
+
+const actions = {
+    login({ commit }, data) {
+        return loginByUserName(data).then(res => {
+            let data = res.data;
+            if (data.code === 0) {
+                commit('setToken', data.data.token);
+                setToken(data.data.token);
+                return data.data.token;
+            } else {
+                return Promise.reject(data.msg);
+            }
+        })
+    },
+    getUserInfo({ commit }, token) {
+        return getUserInfo(token).then(res => {
+            let data = res.data;
+            if (data.code === 0) {
+                commit('setUserInfo', data.data);
+                return data.data;
+            } else {
+                return Promise.reject(data.msg);
+            }
+        })
+    }
+}
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions
+}
