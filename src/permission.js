11ac29558c542ae77f531250bf17a0aeac9b3aa3
JavanShen <1985486089@qq.com>
2022-04-25 11:31:10 +0800

    路由拦截,异步添加路由

--- src/permission.js
+++ src/permission.js
@@ -1,25 +1,41 @@
 import router from './router';
+import store from './store';
 
-// router.beforeEach(async (to, from, next) => {
-//   if (to.path === '/login') {
-//     next();
-//   } else {
-//     const token = getToken();
-//     if (token===undefined) {
-//       next({name:'Login'});
-//     } else {
-//       if(store.getters.isRoute){
-//         next();
-//       }else{
-//         try{
-//           await store.dispatch('getUserInfo',token)
-//           next({...to,replace:true});
-//         }catch(e){
-//           next({name:'Login'});
-//         }
-//       }
-//     }
-//   }
-// });
+import { getToken } from '@/utils/auth';
+
+router.beforeEach(async (to, from, next) => {
+    if (to.path === '/login') {
+        next();
+    } else {
+        const token = getToken();
+        if (token === undefined) {
+            next({ name: 'Login' });
+        } else {
+            if (store.getters.roles?.length > 0) {
+                next();
+            } else {
+                try {
+                    const {roles} = await store.dispatch('user/getUserInfo', token)
+                    const routes = await store.dispatch('permission/generateRoutes', roles)
+
+                    routes.forEach(route => router.addRoute(route))
+                    router.addRoute(
+                        {
+                            path: '*',
+                            name: 'NotFound',
+                            component: () => import('@/views/NotFound.vue'),
+                        }
+                    )
+                    console.log(router.getRoutes())
+
+                    next({ ...to, replace: true });
+                } catch (e) {
+                    console.log(e)
+                    next({ name: 'Login' });
+                }
+            }
+        }
+    }
+});
 
 export default router;
