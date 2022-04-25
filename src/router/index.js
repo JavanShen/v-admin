f37c40fe4ccb682bc9dfaeacc8550d94643cb4f4
JavanShen <1985486089@qq.com>
2022-04-25 11:34:36 +0800

    测试嵌套路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import {getToken} from '@/utils/auth';
-import store from '@/store';
-import {fixedRoutes,asyncRoutes} from '@/router/routes';
+import {fixedRoutes} from '@/router/routes';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes:[...fixedRoutes,...asyncRoutes],
+  routes:[...fixedRoutes],
 });
 
 export default router;
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,12 +2,9 @@ export const fixedRoutes = [
     {
         path: '/login',
         name: 'Login',
-        component: () => import(/* webpackChunkName: "Login" */ '@/views/Login.vue'),
-    },
-    {
-        path: '*',
-        name: 'NotFound',
-        component: () => import(/* webpackChunkName: "NotFound" */ '@/views/NotFound.vue'),
+        component: () => import('@/views/Login.vue'),
+        hidden: true,
+        meta:{icon:'statistics'}
     }
 ]
 
@@ -20,11 +17,80 @@ export const asyncRoutes = [
             {
                 path: 'statistics',
                 name: 'Statistics',
-                component: () => import(/* webpackChunkName: "Statistics" */ '@/views/Statistics.vue'),
-            },{
+                component: () => import('@/views/Statistics.vue'),
+                meta:{title: '统计',icon: 'statistics', roles: ['admin','editor','user']}
+            },
+            {
                 path: 'edit',
                 name: 'Edit',
-                component: () => import(/* webpackChunkName: "Edit" */ '@/views/Edit.vue'),
+                component: () => import('@/views/Edit.vue'),
+                meta: {title: '编辑', icon: 'edit', roles: ['admin','editor']}
+            },
+            {
+                path: 'assets',
+                name: 'Assets',
+                component: () => import('@/views/Edit.vue'),
+                meta: {title: '资产', icon: 'edit', roles: ['admin']},
+                children:[
+                    {
+                        path: 'assets-list',
+                        name: 'AssetsList',
+                        component: () => import('@/views/Edit.vue'),
+                        meta: {title: '资产列表', roles: ['admin']}
+                    }
+                ]
+            },
+            {
+                path: 'user',
+                name: 'User',
+                component: () => import('@/views/Edit.vue'),
+                meta: {title: '用户', icon: 'edit', roles: ['admin']}
+            },
+            {
+                path: 'nester',
+                name: 'Nester',
+                component: () => import('@/views/Edit.vue'),
+                meta: {title: '嵌套测试', icon: 'edit', roles: ['admin']},
+                children: [
+                    {
+                        path: 'nester/nested',
+                        name: 'Nested',
+                        component: () => import('@/views/Edit.vue'),
+                        meta: {title: '子元素1', roles: ['admin'], group: 'first'}
+                    },
+                    {
+                        path: 'nested2',
+                        name: 'Nested2',
+                        component: () => import('@/views/Edit.vue'),
+                        meta: {title: '子元素2', roles: ['admin'], group: 'first'}
+                    },
+                    {
+                        path: 'nested3',
+                        name: 'Nested3',
+                        component: () => import('@/views/Edit.vue'),
+                        meta: {title: '子元素3',roles: ['admin']}
+                    },
+                    {
+                        path: 'nested4',
+                        name: 'Nested4',
+                        component: () => import('@/views/Edit.vue'),
+                        meta: {title: '子元素4', roles: ['admin'], group: 'second'}
+                    },
+                    {
+                        path: 'nested5',
+                        name: 'Nested5',
+                        component: () => import('@/views/Edit.vue'),
+                        meta: {title: '子元素5', roles: ['admin'], group: 'second'},
+                        children: [
+                            {
+                                path: 'nested6',
+                                name: 'Nested6',
+                                component: () => import('@/views/Edit.vue'),
+                                meta: {title: '子元素6', roles: ['admin']}
+                            }
+                        ]
+                    }
+                ]
             }
         ]
     }
