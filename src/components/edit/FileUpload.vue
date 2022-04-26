57c3d3c945e15589ea328e2baf46e05cc7ced4f4
JavanShen <1985486089@qq.com>
2022-04-26 17:19:12 +0800

    增加传参

--- src/components/edit/FileUpload.vue
+++ src/components/edit/FileUpload.vue
@@ -1,47 +1,46 @@
 <template>
-    <div class="upload">
-        <el-upload
-            action="https://jsonplaceholder.typicode.com/posts"
-            ref="upload"
-            :on-preview="handelPreview"
-            :on-remove="handelRemove"
-            :on-change="handelChange"
-            :auto-upload="false"
-            multiple
-            :limit="5"
-            :on-exceed="handelExceed"
-            :file-list="fileList"
-            list-type="picture">
-            <el-button slot="trigger" size="small" type="primary">select file</el-button>
-            <el-button style="margin-left: 10px;" size="small" type="success" @click="upload">upload to server</el-button>
-            <div slot="tip" class="el-upload__tip">files with a size less than 500kb</div>
-        </el-upload>
-    </div>
+    <el-upload
+        :action="uploadApi"
+        :on-change="handelChange"
+        :on-remove="handelRemove"
+        multiple
+        :limit="limit"
+        :file-list="fileList"
+        :auto-upload="false"
+        :list-type="listType">
+        <el-button slot="trigger" size="small" type="primary">select file</el-button>
+        <!-- <el-button style="margin-left: 10px;" size="small" type="success">upload to server</el-button> -->
+        <div slot="tip" class="el-upload__tip">files with a size less than 500kb</div>
+    </el-upload>
 </template>
 
 <script>
 export default {
     name: "FileUpload",
-    data(){
-        return {
-            fileList:[]
+    props: {
+        fileList: {
+            type: Array,
+            default: () => [],
+        },
+        uploadApi: {
+            type: String,
+            default: "",
+        },
+        listType: {
+            type: String,
+            default: "file",
+        },
+        limit: {
+            type: Number,
+            default: 5,
         }
     },
     methods:{
-        handelRemove(file){
-            console.log(file);
-        },
-        handelExceed(files,fileList){
-            console.log(files,fileList);
-        },
-        handelPreview(file){
-            console.log(file);
-        },
         handelChange(file,fileList){
-            console.log(file,fileList);
+            this.$emit("change",file,fileList);
         },
-        upload(){
-            this.$refs.upload.submit()
+        handelRemove(file,fileList){
+            this.$emit('remove',file,fileList);
         }
     }
 }
