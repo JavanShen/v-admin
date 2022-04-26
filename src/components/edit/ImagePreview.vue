a7bd60d1dc989d8d00e5bfb8760c0769282a810d
JavanShen <1985486089@qq.com>
2022-04-26 17:18:39 +0800

    图片预览组件

---
+++ src/components/edit/ImagePreview.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="image-preview">
+        <el-image v-for="image of images" :key="image.id" :src="image.src" fit="fill" style="height: 100px; width: 100px; margin: 0 10px" :preview-src-list="srcList"></el-image>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ImagePreview",
+    props: {
+        images: {
+            type: Array,
+            require: true
+        },
+    },
+    computed:{
+        srcList(){
+            return this.images.map(image=>image.src)
+        }
+    }
+}
+</script>
