f5bba2075bc767b7b22db3e352bc3167e5f3aa3b
JavanShen <1985486089@qq.com>
2022-04-26 17:21:37 +0800

    判断文件类型,预览图片类文件

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="edit">
     <mark-down></mark-down>
-    <file-upload></file-upload>
+    <image-preview :images="images"></image-preview>
+    <file-upload uploadApi="https://jsonplaceholder.typicode.com/posts/" @change="fileSelect" @remove="fileRemove"></file-upload>
   </div>
 </template>
 
 <script>
-import { MarkDown, FileUpload } from "@/components/edit";
+import { MarkDown, FileUpload, ImagePreview } from "@/components/edit";
 
 export default {
   name: "Edit",
   components: {
     MarkDown,
     FileUpload,
+    ImagePreview
   },
+  data(){
+    return {
+      images:[]
+    }
+  },
+  computed: {
+    imagesPromise() {
+      return this.$store.getters.previewImages
+    }
+  },
+  watch: {
+    imagesPromise(val){
+      val.then(images=>{
+        this.images = images;
+      })
+    }
+  },
+  methods:{
+    fileSelect(file,fileList){
+      this.$store.commit('edit/setFiles',fileList);
+    },
+    fileRemove(file,fileList){
+      this.$store.commit('edit/setFiles',fileList);
+    }
+  }
 };
 </script>
