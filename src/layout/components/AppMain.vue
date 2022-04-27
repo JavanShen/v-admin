8ec83fe5a895605c8c8056c08b4c4e72926febf0
JavanShen <1985486089@qq.com>
2022-04-27 15:17:06 +0800

    增加过渡动画和组件缓存

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,20 +1,22 @@
 <template>
-    <section class="container">
-        <transition name="fade-transform" mode="out-in">
-            <router-view></router-view>
-        </transition>
-    </section>
+  <section class="container">
+    <transition name="el-fade-in-linear" mode="out-in">
+      <keep-alive include="Edit">
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
+  </section>
 </template>
 
 <script>
 export default {
-    name:'AppMain',
-}
+  name: "AppMain",
+};
 </script>
 
 <style scoped>
-.container{
-    height: 100%;
-    width: 100%;
+.container {
+  height: 100%;
+  width: 100%;
 }
 </style>
