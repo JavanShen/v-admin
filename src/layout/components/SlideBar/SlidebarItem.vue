fef8a28f2d8f0038fc2be69c47ca2df940aa92a6
JavanShen <1985486089@qq.com>
2022-04-28 13:56:41 +0800

    修复页面路径问题

--- src/layout/components/SlideBar/SlidebarItem.vue
+++ src/layout/components/SlideBar/SlidebarItem.vue
@@ -1,36 +1,39 @@
 <template>
     <div v-if="menus.hidden"></div>
+
     <el-submenu v-else-if="menus.children" :index="index">
       <template slot="title">
         <icon-svg
-          v-if="menus.meta.icon"
+          v-if="icon"
           class="icon"
-          :iconClass="menus.meta.icon"
+          :icon-class="icon"
         ></icon-svg>
-        <span slot="title">{{ menus.meta.title }}</span>
+        <span slot="title">{{ title }}</span>
       </template>
       <slidebar-item
         :key="menu.path"
         v-for="(menu, i) in menus.children"
         :menus="menu"
         :index="index + '-' + i"
+        :base-url="path"
       ></slidebar-item>
     </el-submenu>
 
-    <Link v-else :to="menus.path">
-      <el-menu-item :index="index" :key="menus.path">
+    <Link v-else :to="path">
+      <el-menu-item :index="index" :key="path">
         <icon-svg
-          v-if="menus.meta.icon"
-          :iconClass="menus.meta.icon"
-          class="icon"
-        ></icon-svg>
-        <span slot="title">{{ menus.meta.title }}</span>
+        v-if="icon"
+        :icon-class="icon"
+        class="icon"
+      ></icon-svg>
+        <span slot="title">{{ title }}</span>
       </el-menu-item>
     </Link>
 </template>
 
 <script>
 import Link from "./Link.vue";
+import {isExternal} from "@/utils/validate";
 
 export default {
   name: "SlidebarItem",
@@ -46,6 +49,23 @@ export default {
       type: String,
       default: "",
     },
+    baseUrl: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    path() {
+      if(isExternal(this.baseUrl)) return this.baseUrl
+      if(isExternal(this.menus.path)) return this.menus.path
+      return this.baseUrl+'/'+this.menus.path;
+    },
+    icon() {
+      return this.menus.meta.icon;
+    },
+    title() {
+      return this.menus.meta.title;
+    },
   },
 };
 </script>
@@ -55,7 +75,7 @@ export default {
   margin-right: 10px;
 }
 
-.el-menu--collapse .icon{
+.el-menu--collapse .icon {
   margin-right: 0;
 }
 </style>
