18d151c2c95fbcbe494bbd5a591c6e56b932990b
JavanShen <1985486089@qq.com>
2022-04-28 13:57:34 +0800

    清理

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 
 import router from './router';
-import permission from './permission';
+import './permission';
 
 import store from './store';
 
