31f0903730d82ad85e23bc1e6013c1df38159986
JavanShen <1985486089@qq.com>
2022-05-04 19:37:45 +0800

    新增用户数据更新接口

--- mock/user.js
+++ mock/user.js
@@ -5,10 +5,24 @@ Mock.mock('/user/list',{
     'data|100':[{
         'id|+1':1,
         'name':'@cname',
-        'age|1-100':1,
         'birthday':'@date',
         'address':'@county(true)',
         'email':'@email',
         'phone':'@integer(1,1000000000)'
     }]
+})
+
+Mock.mock('/user/update',config=>{
+    const {id,name,birthday,address,email,phone}=JSON.parse(config.body);
+    return {
+        'code':0,
+        'data':{
+            'id':id,
+            'name':name,
+            'birthday':birthday,
+            'address':address,
+            'email':email,
+            'phone':phone
+        }
+    }
 })
