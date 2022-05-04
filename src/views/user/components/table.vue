94e085d48e8c0bbb4242c51f73afc40a26fac777
JavanShen <1985486089@qq.com>
2022-05-04 19:41:47 +0800

    用户列表组件

---
+++ src/views/user/components/table.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-table :data="tableData" height="610" border stripe v-loading="loading">
+    <el-table-column
+      v-for="col in columns"
+      :prop="col.id"
+      :key="col.id"
+      :label="col.label"
+      :width="col.width"
+      :sortable="col.id === 'age'"
+    >
+    </el-table-column>
+    <el-table-column label="操作" width="100">
+      <template slot-scope="scope">
+        <el-button @click="$emit('edit',scope.row,scope.$index,tableData)" type="text" size="small"
+          >编辑</el-button
+        >
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: "user-table",
+  props: {
+    tableData: {
+      type: Array,
+      default: () => [],
+    },
+    columns: {
+      type: Array,
+      default: () => [],
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
