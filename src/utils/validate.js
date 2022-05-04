10b4117c405dcab18575ed44b24eb9763f86d80f
JavanShen <1985486089@qq.com>
2022-05-04 19:39:49 +0800

    新增对象相同判断和数据类型判断

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -11,3 +11,28 @@ export function isExternal(path){
 export function isSpecifedSize(file,size){
     return file.size<=size;
 }
+
+export function isSameObject(obj1,obj2,isStrict=false){
+    if(isStrict){
+        return JSON.stringify(obj1)===JSON.stringify(obj2);
+    }else{
+        const isSame=(obj)=>{
+            for(let key in obj){
+                if(typeOf(obj[key])==='object'){
+                    if(!isSame(obj[key])){
+                        return false;
+                    }
+                }else if(obj[key]!==obj2[key]){
+                    return false;
+                }
+            }
+            return true
+        }
+
+        return isSame(obj1);
+    }
+}
+
+export function typeOf(obj){
+    return Object.prototype.toString.call(obj).slice(8,-1).toLowerCase();
+}
