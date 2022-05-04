899467f45c2e9f64a7a92c46e7249a1b4d85d485
JavanShen <1985486089@qq.com>
2022-05-04 19:38:59 +0800

    时间操作工具类

---
+++ src/utils/date.js
@@ -0,0 +1,15 @@
+export function getDate(date) {
+  return new Date(date).toLocaleDateString();
+}
+
+export function betweenDate(start,end,type='year'){
+    let startDate=new Date(start),
+        endDate=new Date(end);
+    if(type==='year'){
+        return endDate.getFullYear()-startDate.getFullYear();
+    }else if(type==='month'){
+        return (endDate.getFullYear()-startDate.getFullYear())*12+(endDate.getMonth()-startDate.getMonth());
+    }else if(type==='day'){
+        return (endDate.getFullYear()-startDate.getFullYear())*365+(endDate.getMonth()-startDate.getMonth())*30+(endDate.getDate()-startDate.getDate());
+    }
+}
