fc34694f4582aa0341a1648185c8e8917c45b22a
JavanShen <1985486089@qq.com>
2022-05-07 11:06:06 +0800

    封装echart配置

--- src/utils/echarts.js
+++ src/utils/echarts.js
@@ -1,19 +1,21 @@
-export function options(data={},type='bar') {
-    return {
-        dataset:{
-            source:data
-        },
-        xAxis: {
-            type: 'category'
-        },
-        yAxis: {},
-        dataset: {},
-        series: [
-            {
-                type: "bar",
+export const options= {
+    bar({data}){
+        let barNum=0
+        if(Array.isArray(data[0])){
+            barNum=data[0].length-1
+        }else{
+            barNum=Object.keys(data[0]).length-1
+        }
+
+        return {
+            legend: {},
+            tooltip: {},
+            dataset: {
+                source: data
             },
-        ],
-        tooltip: {},
-        legend: {},
+            xAxis: {type: 'category'},
+            yAxis: {},
+            series: new Array(barNum).fill({type: 'bar'})
+        }
     }
 }
