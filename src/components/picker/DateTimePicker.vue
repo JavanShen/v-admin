584b8ea3ab4158c14abe0d58265870b7170b7d4c
JavanShen <1985486089@qq.com>
2022-05-11 09:08:15 +0800

    新增日期时间选择组件

---
+++ src/components/picker/DateTimePicker.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-date-picker :value="time" type="datetime" size="normal" placeholder="选择日期时间">
+    </el-date-picker>
+    
+</template>
+
+<script>
+export default {
+    name: "DateTimePicker",
+    props: {
+        time: {
+            type: Date,
+            default: () => new Date()
+        },
+    }
+}
+</script>
