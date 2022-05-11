80f2f88e0c5643e5452bb4a816db4223710ecd0b
JavanShen <1985486089@qq.com>
2022-05-11 09:09:24 +0800

    新增待办页面

---
+++ src/views/todo/components/task.vue
@@ -0,0 +1,71 @@
+<template>
+    <el-card :shadow="shadowType" :body-style="{ padding: '20px' }">
+        <el-row type="flex" align="middle">
+            <el-col :span="1" :offset="0">
+                <el-checkbox
+                    :value="isComplete"
+                    :indeterminate="false"
+                    @change="handelCheckChange"
+                ></el-checkbox>
+            </el-col>
+            <el-col :span="23" :offset="0">
+                <el-input
+                    v-model="taskContent"
+                    placeholder="新建任务"
+                    size="normal"
+                    clearable
+                    @change="handelInputChange"
+                ></el-input>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="8" :offset="0">
+                <date-time-picker class="date-time-picker"></date-time-picker>
+            </el-col>
+            
+        </el-row>
+    </el-card>
+</template>
+
+<script>
+import DateTimePicker from "@/components/picker/DateTimePicker";
+
+export default {
+    name: "Task",
+    components: {
+        DateTimePicker
+    },
+    props: {
+        shadowType: {
+            type: String,
+            default: "never",
+        },
+        isComplete: {
+            type: Boolean,
+            default: false,
+        },
+        taskContent: {
+            type: String,
+            default: "",
+        },
+    },
+    methods: {
+        handelInputChange(val) {
+            this.$emit("input", val);
+        },
+        handelCheckChange(val) {
+            this.$emit("update:isComplete", val);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.date-time-picker {
+    width: 100%;
+}
+
+.el-row:not(:last-child) {
+    margin-bottom: 10px;
+}
+</style>
---
+++ src/views/todo/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="todo">
+        <Task :isComplete.sync="isComplete"></Task>
+
+        <el-drawer
+            title="日程安排"
+            :visible.sync="isOpen"
+            direction="rtl"
+            size="660px"
+            :destroy-on-close="true"
+            :show-close="true"
+            :wrapperClosable="true"
+        >
+            <Calendar :dates="dates"></Calendar>
+        </el-drawer>
+    </div>
+</template>
+
+<script>
+import Calendar from "@/components/date/Calendar";
+import Task from "./components/task";
+
+export default {
+    name: "Todo",
+    components: {
+        Calendar,
+        Task
+    },
+    data() {
+        return {
+            isOpen: false,
+            dates: "repeat",
+
+            isComplete: false,
+        };
+    },
+    methods: {
+        open() {
+            this.isOpen = !this.isOpen;
+        },
+    },
+};
+</script>
