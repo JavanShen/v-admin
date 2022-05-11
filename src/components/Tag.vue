14f077cb464b7e8a73f5f54f82a536b42e7dec7b
JavanShen <1985486089@qq.com>
2022-05-11 09:09:38 +0800

    新增标签组件

---
+++ src/components/Tag.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="tag-group">
+        <el-tag
+            v-for="tag in tagData"
+            :key="tag.id"
+            size="normal"
+            effect="light"
+            closable="tag.closable===undefined?true:tag.closable"
+            @close="handelClose"
+        >{{tag.name}}</el-tag>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Tag",
+    props: {
+        tagData: {
+            type: Array,
+            require: true,
+        },
+    },
+    methods: {
+        handelClose() {
+            this.$emit("close");
+        },
+    },
+};
+</script>
