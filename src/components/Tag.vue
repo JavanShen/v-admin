bac2e99e2b1f8eae7511a1963b77982eb67ecfc7
JavanShen <1985486089@qq.com>
2022-05-12 09:11:04 +0800

    新增标签组件

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -1,14 +1,32 @@
 <template>
     <div class="tag-group">
         <el-tag
-            v-for="tag in tagData"
-            :key="tag.id"
+            v-for="tag in tagList"
+            :key="tag.name"
             size="normal"
             effect="light"
-            closable="tag.closable===undefined?true:tag.closable"
-            @close="handelClose"
-        >{{tag.name}}</el-tag>
-        
+            :disable-transitions="true"
+            :closable="tag.closable === undefined ? true : tag.closable"
+            @close="handelClose(tag.name)"
+            >{{ tag.name }}</el-tag
+        >
+        <el-input
+            v-if="inputVisible"
+            v-model="searchVal"
+            size="small"
+            ref="saveTagInput"
+            class="input-new-tag"
+            @keyup.enter.native="handelInputConfirm"
+            @blur="handelInputConfirm"
+        ></el-input>
+        <el-button
+            v-else
+            type="primary"
+            plain
+            class="button-new-tag"
+            @click="showInput"
+            >+ 标签</el-button
+        >
     </div>
 </template>
 
@@ -18,13 +36,79 @@ export default {
     props: {
         tagData: {
             type: Array,
-            require: true,
+            default: () => [],
+        },
+        tagHistory: {
+            type: Array,
+            default: () => [],
+        },
+    },
+    data() {
+        return {
+            searchVal: "",
+            tagList: [],
+            inputVisible: false,
+        };
+    },
+    watch: {
+        tagData() {
+            this.tagList = [...this.tagList, ...this.tagData];
         },
     },
     methods: {
-        handelClose() {
-            this.$emit("close");
+        handelClose(tag) {
+            this.tagList.splice(
+                this.tagList.findIndex((item) => item.name === tag),
+                1
+            );
+        },
+        handelInputConfirm() {
+            const { searchVal } = this;
+
+            if (searchVal !== "") {
+                if(this.tagList.findIndex(item=>item.name===searchVal)>-1) return this.$message.error("标签已存在");
+                this.tagList.push({
+                    name: searchVal,
+                    closable: true,
+                });
+            }
+
+            this.inputVisible = false;
+            this.searchVal = "";
+        },
+        showInput() {
+            this.inputVisible = true;
+            this.$nextTick(() => {
+                this.$refs.saveTagInput.focus();
+            });
         },
     },
 };
 </script>
+
+<style scoped>
+.tag-group {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    width: 100%;
+}
+.el-tag + .el-tag {
+    margin-left: 10px;
+}
+.el-tag{
+    margin-top: 5px;
+    margin-bottom: 5px;
+}
+.button-new-tag {
+    margin:5px 0 5px 10px;
+    height: 32px;
+    line-height: 30px;
+    padding: 0 14px;
+}
+.input-new-tag{
+    width: 70px;
+    margin: 5px 0 5px 10px;
+    vertical-align: bottom;
+}
+</style>
