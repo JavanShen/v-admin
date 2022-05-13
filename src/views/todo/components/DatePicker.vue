f6c4ca25900c64044097696a22796b06381d793e
JavanShen <1985486089@qq.com>
2022-05-13 22:47:45 +0800

    新增待办日期选择组件

---
+++ src/views/todo/components/DatePicker.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-popover
+        placement="bottom"
+        trigger="click">
+        <date-panel v-model="date" :show-time="false" @change="handelChange"></date-panel>
+        <slot slot="reference"></slot>
+    </el-popover>
+</template>
+
+<script>
+import DatePanel from "@/components/date/DatePanel.vue";
+
+export default {
+    name: "DatePicker",
+    components: {
+        DatePanel,
+    },
+    data() {
+        return {
+            date: new Date(),
+        };
+    },
+    created() {
+        this.$emit("change", this.date);
+    },
+    methods: {
+        handelChange(date) {
+            this.$emit("change", date);
+        },
+    },
+}
+</script>
