b5b3a3ca2560a9a7a7e1305c65dec18df326c264
JavanShen <1985486089@qq.com>
2022-05-13 22:46:43 +0800

    新增日期选择器组件

---
+++ src/components/picker/DatePicker.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-date-picker :value="date" type="date" :size="size" placeholder="选择日期">
+    </el-date-picker>
+</template>
+
+<script>
+export default {
+    name: "DatePicker",
+    props: {
+        date: {
+            type: Date,
+            default: () => new Date()
+        },
+        size: {
+            type: String,
+            default: "normal"
+        },
+    },
+}
+</script>
--- src/components/picker/DateTimePicker.vue
+++ src/components/picker/DateTimePicker.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-date-picker :value="time" type="datetime" :size="size" placeholder="选择日期时间">
+    <el-date-picker :value="time" type="datetime" :picker-options="pickerOptions" :size="size" placeholder="选择日期时间">
     </el-date-picker>
     
 </template>
@@ -16,6 +16,36 @@ export default {
             type: String,
             default: "normal"
         },
+    },
+    data() {
+        return {
+            pickerOptions: {
+                shortcuts: [
+                    {
+                        text: "今天",
+                        onClick(picker) {
+                            picker.$emit("pick", new Date());
+                        }
+                    },
+                    {
+                        text: "昨天",
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setTime(date.getTime() - 3600 * 1000 * 24);
+                            picker.$emit("pick", date);
+                        }
+                    },
+                    {
+                        text: "一周前",
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit("pick", date);
+                        }
+                    }
+                ]
+            },
+        }
     }
 }
 </script>
