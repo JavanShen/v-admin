d05a6a9ba1e0e43702f02b40a5018d51e32b3b06
JavanShen <1985486089@qq.com>
2022-05-13 22:44:52 +0800

    日期工具类扩充

--- src/utils/date.js
+++ src/utils/date.js
@@ -2,6 +2,22 @@ export function getDate(date) {
   return new Date(date).toLocaleDateString();
 }
 
+export function getDateZh(date) {
+    return date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日';
+}
+
+export function isSameYear(date1,date2=new Date()) {
+    return date1.getFullYear() == date2.getFullYear();
+}
+
+export function isSameMonth(date1,date2=new Date()) {
+    return date1.getFullYear() == date2.getFullYear() && date1.getMonth() == date2.getMonth();
+}
+
+export function isSameDay(date1,date2=new Date()) {
+    return date1.getFullYear() == date2.getFullYear() && date1.getMonth() == date2.getMonth() && date1.getDate() == date2.getDate();
+}
+
 export function betweenDate(start,end,type='year'){
     let startDate=new Date(start),
         endDate=new Date(end);
@@ -13,6 +29,8 @@ export function betweenDate(start,end,type='year'){
         return (endDate.getFullYear()-startDate.getFullYear())*365+(endDate.getMonth()-startDate.getMonth())*30+(endDate.getDate()-startDate.getDate());
     }
 }
+
+
 
 export const today=new Date()
 
