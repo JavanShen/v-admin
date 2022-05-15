bf6763534accc051e383cf546795d23602e4e9c6
JavanShen <1985486089@qq.com>
2022-05-15 22:52:48 +0800

    新增标签,标签历史增删功能,类型判断

--- src/views/todo/components/task.vue
+++ src/views/todo/components/task.vue
@@ -10,6 +10,8 @@
             <el-col :span="23" :offset="0">
                 <el-input
                     v-model="taskContent"
+                    :class="{'disabled-input': type==='view'}"
+                    :readonly="type==='view'"
                     placeholder="新建任务"
                     size="normal"
                     clearable
@@ -18,12 +20,22 @@
         </el-row>
         <el-row>
             <el-col :span="24">
-                <Tag :tag-data="tagData" :tag-history="tagHistory" @addTag="addTag"></Tag>
+                <Tag
+                    :tag-data="tagData"
+                    :tag-history="tagHistory"
+                    @addTag="addTag"
+                    @chooseTag="chooseTag"
+                    @deleteTag="deleteTag"
+                    @deleteHistoryTag="deleteHistoryTag"
+                ></Tag>
             </el-col>
         </el-row>
-        <el-row class="info" type="flex" align="middle">
+        <el-row class="info" type="flex" justify="space-between" align="middle">
             <el-col :span="8" :offset="0">
-                <Info @change="changeInfo"></Info>
+                <Info :info-date="info.date" :info-repeat="info.repeat" @change="changeInfo"></Info>
+            </el-col>
+            <el-col :span="1" :offset="0">
+                <el-button v-if="type==='create'" type="primary" class="add-btn" size="small" circle icon="el-icon-plus" @click="addTask"></el-button>
             </el-col>
         </el-row>
     </el-card>
@@ -32,7 +44,9 @@
 <script>
 import DatePicker from "@/components/picker/DatePicker.vue";
 import Tag from "@/components/Tag";
-import Info from "./Info.vue"
+import Info from "./Info.vue";
+
+import { addTag, deleteTag, getHistory } from "@/api/todo";
 
 export default {
     name: "Task",
@@ -41,10 +55,23 @@ export default {
         Tag,
         Info,
     },
+    provide() {
+        return {
+            isEdit: this.type === 'create' || this.type === 'edit',
+        };
+    },
     props: {
         shadowType: {
             type: String,
             default: "never",
+        },
+        type: {
+            type: String,
+            default: 'create',
+        },
+        taskData: {
+            type: Object,
+            default: () => ({}),
         },
     },
     data() {
@@ -55,16 +82,123 @@ export default {
             tagData: [],
             tagHistory: [],
 
-            info:{}
+            info: {
+                date: new Date(),
+                repeat: 0
+            },
+        };
+    },
+    created() {
+        if(this.type==='edit'||this.type==='view'){
+            let {taskContent,isComplete,tagData,info}=this.taskData;
+            this.taskContent = taskContent||'';
+            this.isComplete = isComplete||false;
+            this.tagData = tagData||[];
+            this.info = info?{...info,date:new Date(info.date)}:{
+                date: new Date(),
+                repeat: 0
+            };
         }
+        this.getHistoryTag();
     },
     methods: {
         changeInfo(info) {
-            this.info=info;
+            this.info = info;
         },
-        addTag(){
-            
-        }
+        async getHistoryTag() {
+            const { data: res } = await getHistory();
+            if (res.code === 0) {
+                this.tagHistory = [...res.data];
+            }
+        },
+        async deleteHistoryTag(id) {
+            const { data: res } = await deleteTag(id);
+            if (res.code === 0) {
+                this.tagHistory.splice(this.searchHistoryTagById(id), 1);
+            } else {
+                this.$message.error(res.msg);
+            }
+        },
+        deleteTag(id) {
+            this.tagData.splice(this.searchTagById(id), 1);
+            this.tagHistory[this.searchHistoryTagById(id)].selected = false;
+        },
+        addTag(tags, isNew) {
+            if (isNew) {
+                this.addTagsAsync(tags).then((res) => {
+                    res.forEach((item) => {
+                        if (item.code === 0) {
+                            this.tagData.push(item.data);
+
+                            let index = this.searchHistoryTagById(item.data.id);
+                            index > -1
+                                ? (this.tagHistory[index].selected = true)
+                                : this.tagHistory.push({
+                                      ...item.data,
+                                      selected: true,
+                                  });
+                        }
+                    });
+                });
+            } else {
+                this.tagData = [...this.tagData, ...tags];
+            }
+        },
+        addTagsAsync(tags) {
+            let promiseArr = tags.map((tag) => {
+                return new Promise((resolve, reject) => {
+                    addTag(tag)
+                        .then((res) => {
+                            resolve({ ...res.data, name: tag });
+                        })
+                        .catch((err) => {
+                            reject(err);
+                        });
+                });
+            });
+            return Promise.all(promiseArr);
+        },
+        chooseTag(id) {
+            let index = this.searchHistoryTagById(id);
+            if (index > -1) {
+                let type = this.tagHistory[index].selected;
+                this.tagHistory.splice(index, 1, {
+                    ...this.tagHistory[index],
+                    selected: !type,
+                });
+
+                if (!type) {
+                    this.tagData.push(this.tagHistory[index]);
+                } else {
+                    let index = this.searchTagById(id);
+                    index > -1 && this.tagData.splice(index, 1);
+                }
+            }
+        },
+        addTask(){
+            if(this.taskContent){
+                this.$emit("addTask", {
+                    isComplete: this.isComplete,
+                    taskContent: this.taskContent,
+                    tagData: this.tagData,
+                    info: this.info,
+                });
+                this.taskContent = "";
+            }
+        },
+        //utils
+        searchTagById(id, isIndex = true) {
+            if (isIndex) {
+                return this.tagData.findIndex((item) => item.id === id);
+            }
+            return this.tagData.find((item) => item.id === id);
+        },
+        searchHistoryTagById(id, isIndex = true) {
+            if (isIndex) {
+                return this.tagHistory.findIndex((item) => item.id === id);
+            }
+            return this.tagHistory.find((item) => item.id === id);
+        },
     },
 };
 </script>
@@ -80,5 +214,16 @@ export default {
 
 .info {
     text-align: left;
+}
+
+.add-btn{
+    box-shadow: 0 1px 2px 1px rgba(214, 214, 214, 0.7);
+}
+
+.disabled-input >>> input{
+    cursor: default;
+    border: none;
+    font-size: 16px;
+    padding: 0;
 }
 </style>
