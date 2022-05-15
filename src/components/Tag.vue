9acdd1e43d1a374fb581690f94cfffa930c87299
JavanShen <1985486089@qq.com>
2022-05-15 22:50:15 +0800

    逻辑修改,增加状态

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -6,11 +6,11 @@
             size="normal"
             effect="light"
             :disable-transitions="true"
-            :closable="tag.closable === undefined ? true : tag.closable"
+            :closable="isEdit?(tag.closable === undefined ? true : tag.closable):false"
             @close="deleteTag(tag.id)"
             >{{ tag.name }}</el-tag
         >
-        <el-popover placement="bottom" trigger="click">
+        <el-popover v-if="isEdit" placement="bottom" width="220" trigger="click">
             <el-input
                 v-model="newTagName"
                 placeholder="新增标签"
@@ -22,12 +22,16 @@
             <el-tag
                 v-for="tag in tagHistory"
                 :key="tag.id"
-                type="info"
+                class="tag-history"
+                :type="tag.selected ? 'primary' : 'info'"
                 size="normal"
-                effect="light"
+                :effect="tag.selected ? 'dark' : 'light'"
+                :disable-transitions="true"
                 :closable="tag.closable === undefined ? true : tag.closable"
                 @close="deleteHistoryTag(tag.id)"
-            >{{tag.name}}</el-tag>
+                @click="chooseTag(tag.id)"
+                >{{ tag.name }}</el-tag
+            >
 
             <el-button
                 type="primary"
@@ -54,6 +58,7 @@ export default {
             default: () => [],
         },
     },
+    inject: ['isEdit'],
     data() {
         return {
             newTagName: "",
@@ -64,21 +69,24 @@ export default {
             this.$emit("deleteTag", id);
         },
         deleteHistoryTag(id) {
-            this.$emit('deleteHistoryTag', id);
+            this.$emit("deleteHistoryTag", id);
         },
         handelInputConfirm() {
             const { newTagName } = this;
 
             if (newTagName !== "") {
                 if (
-                    this.tagList.findIndex((item) => item.name === newTagName) >
+                    this.tagData.findIndex((item) => item.name === newTagName) >
                     -1
                 )
                     return this.$message.error("标签已存在");
-                
-                this.$emit("addTag", newTagName);
+
+                this.$emit("addTag", [newTagName], true);
             }
             this.newTagName = "";
+        },
+        chooseTag(id) {
+            this.$emit("chooseTag", id);
         },
     },
 };
@@ -92,7 +100,7 @@ export default {
     width: 100%;
 }
 
-.el-tag + .el-tag {
+.el-tag ~ .el-tag {
     margin-left: 10px;
 }
 
@@ -106,5 +114,13 @@ export default {
     height: 32px;
     line-height: 30px;
     padding: 0 14px;
+}
+
+.tag-history {
+    cursor: pointer;
+}
+
+.tag-history:hover {
+    border: 1px solid #41b883;
 }
 </style>
