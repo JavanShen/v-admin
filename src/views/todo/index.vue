ee4a7d9c7fec80ab01e466f2afdd48a4488478b9
JavanShen <1985486089@qq.com>
2022-05-15 22:53:55 +0800

    新增任务列表和任务新建,页面布局调整

--- src/views/todo/index.vue
+++ src/views/todo/index.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="todo">
-        <Task></Task>
+        <Task class="create-task" type="create" shadow-type="hover" @addTask="addTask"></Task>
 
+        <el-divider>
+            <el-button type="primary" plain circle size="small" @click="open" icon="el-icon-date"></el-button>
+        </el-divider>
+
+        <Task class="view-task" v-for="task in tasks" shadow-type="hover" type="view" :task-data="task" :key="task.id"></Task>
         <el-drawer
             title="日程安排"
             :visible.sync="isOpen"
@@ -19,6 +24,7 @@
 <script>
 import Calendar from "@/components/date/Calendar";
 import Task from "./components/Task";
+import {getTasks, addTask} from '@/api/todo';
 
 export default {
     name: "Todo",
@@ -30,12 +36,40 @@ export default {
         return {
             isOpen: false,
             dates: "repeat",
+
+            tasks: []
         };
+    },
+    created() {
+        this.getTasks();
     },
     methods: {
         open() {
             this.isOpen = !this.isOpen;
         },
+        addTask(info) {
+            addTask(info).then(({data:res}) => {
+                if (res.code === 0) {
+                    this.getTasks();
+                }
+            });
+        },
+        getTasks(){
+            getTasks().then(res => {
+                let {data} = res.data
+                this.tasks = data;
+            });
+        }
     },
 };
 </script>
+
+<style scoped>
+.view-task {
+    margin-bottom: 10px;
+}
+
+.open-btn {
+    cursor: pointer;
+}
+</style>
