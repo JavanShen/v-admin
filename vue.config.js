8481cf73fc88b487beb730ea95c5c6484afde23f
JavanShen <1985486089@qq.com>
2022-06-23 09:29:37 +0800

    :wrench: 设置公共路径

--- vue.config.js
+++ vue.config.js
@@ -1,39 +1,40 @@
-const path=require('path');
-const resolve=(dir) => {
+const path = require('path');
+const resolve = (dir) => {
     return path.join(__dirname, dir);
 }
 
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-element-admin/' : '/',
     lintOnSave: false,
-    chainWebpack: config=>{
+    chainWebpack: config => {
         config.module
-        .rule('svg')
-        .exclude.add(resolve('src/IconSvg'))
-        .end()
+            .rule('svg')
+            .exclude.add(resolve('src/IconSvg'))
+            .end()
 
         config.module
-        .rule('icons')
-        .test(/\.svg$/)
-        .include.add(resolve('src/IconSvg'))
-        .end()
-        .use('svg-sprite-loader')
-        .loader('svg-sprite-loader')
-        .options({
-            symbolId: 'icon-[name]'
-        })
-        .end()
+            .rule('icons')
+            .test(/\.svg$/)
+            .include.add(resolve('src/IconSvg'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]'
+            })
+            .end()
 
         config.module
-        .rule('compile')
-        .test(/\.js$/)
-        .include
+            .rule('compile')
+            .test(/\.js$/)
+            .include
             .add(resolve('node_modules/element-ui/packages'))
             .add(resolve('node_modules/element-ui/src'))
             .end()
-        .exclude
+            .exclude
             .add(resolve('node_modules/element-ui/src/utils/date.js'))
             .end()
-        .use('babel')
-        .loader('babel-loader')
+            .use('babel')
+            .loader('babel-loader')
     }
 }
